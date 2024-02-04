2eb8ce73b6226a0c9129a14cf040fe14141f9c2a
Wang Lu <cnwanglu@icloud.com>
2024-02-05 00:17:12 +0800

    Finish lab10

--- lab10/lab10.scm
+++ lab10/lab10.scm
@@ -1,10 +1,24 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+  (cond 
+    ((= num1 num2) 0)
+    ((> num1 num2) 1)
+    (else -1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num)
+  (lambda (inc) (+ num inc)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+  (lambda (x) 
+    (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) 
+  (lambda (x)
+    (if (= n 1)
+      (f x)
+      ((composed f (repeat f (- n 1))) x)
+    )
+  )
+)
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +30,11 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+  (define big (max a b))
+  (define small (min a b))
+  (if (zero? small)
+    big
+    (gcd small (modulo big small))
+  )
+)
